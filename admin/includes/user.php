525e399cbd707925fb3c063cec7f8f77c3516dee
Fatlinda <fatlindabrahaj@gmail.com>
2023-06-23 23:19:46 +0200

    .

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -2,6 +2,14 @@
 
 class User{
 
+
+
+        public $id;
+        public $username;
+        public $password;
+        public $first_name;
+        public $last_name;
+
         public static function find_all_users(){
 
               return self::find_this_query('SELECT * FROM users');
@@ -11,11 +19,9 @@ class User{
 
         public static function find_user_by_id($user_id){
             global $database;
-            $result_set= self::find_this_query('SELECT * FROM users WHERE id= $user_id');  
-            $found_user=mysqli_fetch_array($result_set);
+            $result_set = self::find_this_query("SELECT * FROM users WHERE id = {$user_id}");  
+            $found_user = mysqli_fetch_array($result_set);
             return $found_user;          
-       
-       
         }
 
         public static function find_this_query($sql){
