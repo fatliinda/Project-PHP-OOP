e4c30cfc028e80d7aefefe2e78f68aa8b0b9eeaf
Fatlinda <fatlindabrahaj@gmail.com>
2023-06-27 00:30:14 +0200

    .loginpage

---
+++ admin/login.php
@@ -0,0 +1,39 @@
+<?php
+
+    require_once('init.php');
+
+?>
+
+
+<?php
+
+    if(!$session->is_signed_in()){redirect("index.php");}
+
+    if(isset($_POST['submit'])){
+
+
+        $username=trim($_POST['username']);
+        $password=trim($_POST['password']);
+
+    }
+
+   $user_found= User::verify_user($username,$password);
+
+
+    if($user_found){
+
+            $session->login($user_found);
+            redirect('index.php');
+    }
+    else {
+
+        $the_message="your password or username is incorrect";
+    }
+
+
+
+    
+
+
+
+?>
