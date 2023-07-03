7cd1241543300bc6e793437f99d5bba7257dedae
Fatlinda <fatlindabrahaj@gmail.com>
2023-07-03 15:53:44 +0200

    .photosclass

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -3,7 +3,7 @@
 
 <?php
 
-class User{
+class User extends Db_object{
 
 
         protected static $db_table='users';
@@ -14,35 +14,11 @@ class User{
         public $first_name;
         public $last_name;
 
-        public static function find_all(){
-
-            return self::find_this_query("SELECT * FROM " . self::$db_table);
-
-        }
+        
 
 
-        public static function find_by_id($user_id){
-            global $database;
-           
+        
 
-            $the_result_array = self::find_this_query("SELECT * FROM " . self::$db_table . " WHERE id = $user_id LIMIT 1");
-
-             return !empty($the_result_array) ? array_shift($the_result_array): false;
-        }
-
-        public static function find_this_query($sql){
-
-            global $database;
-            $result_set=$database->query($sql);
-            $the_object_array=array();
-            while($row= mysqli_fetch_array($result_set)){
-                    $the_object_array[]= self::instantiation($row);
-
-
-            }
-           
-            return $the_object_array;
-        }
 
 
         public static function verify_user($username, $password){
@@ -65,26 +41,9 @@ return !empty($the_result_array) ? array_shift($the_result_array): false;
 
 
 
-        public static function instantiation($the_record){
-            $the_object = new self;
+       
         
-            foreach($the_record as $the_attribute => $value) {
-                if($the_object->has_the_attribute($the_attribute)) {
-                    $the_object->$the_attribute = $value;
-                }
-            }
-        
-            return $the_object;
-        }
-        
-    private function has_the_attribute($the_attribute){
-
-                $object_properties=get_object_vars($this);
-
-             return   array_key_exists($the_attribute,$object_properties);
-
-
-    }
+   
 
 
 
