08db0588e5140925fe9a1f512be7b567a9077f16
Fatlinda <fatlindabrahaj@gmail.com>
2023-07-07 00:13:28 +0200

    .

--- admin/includes/delete_photo.php
+++ admin/includes/delete_photo.php
@@ -4,5 +4,22 @@
         redirect('login.php');
       }
 ?>
+<?php 
 
+      if (empty($_GET['photo_id'])){
+
+        redirect('photos.php');
+      }
+$photo= Photo::find_by_id($_GET['photo_id']);
+
+if($photo){
+$photo->delete_photo();
+
+
+
+
+}
+
+
+?>
       
